974766aa03d78a07267fb02a94cfbdbcf7b6beba
rksam <erksg.10.26@gmail.com>
2025-01-18 01:58:09 -0500

    responsive

---
+++ main.js
@@ -0,0 +1,27 @@
+document.addEventListener("scroll", () => {
+  const sections = document.querySelectorAll(".section");
+
+  sections.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top;
+    const sectionBottom = section.getBoundingClientRect().bottom;
+    const windowHeight = window.innerHeight;
+
+    // Agregar la clase si el elemento entra al viewport
+    if (sectionTop < windowHeight - 100 && sectionBottom > 100) {
+      section.classList.add("visible");
+    } else {
+      // Quitar la clase si el elemento sale del viewport
+      section.classList.remove("visible");
+    }
+  });
+});
+
+// BOTON CV
+const cv = document.getElementById("cv");
+
+cv.addEventListener("click", () => {
+  const file = document.createElement("a");
+  file.href = "./file/CV_Rksam.pdf";
+  file.download = "CV_Rksam.pdf";
+  file.click();
+});
